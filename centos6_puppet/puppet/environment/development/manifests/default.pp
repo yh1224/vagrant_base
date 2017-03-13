7dcc439a0e0140984e59d61d249cfd8e641f0978
Yoshiharu Hirose <yh1224@gmail.com>
2017-03-14 00:11:44 +0900

    Use absolute path

--- centos6_puppet/puppet/environment/development/manifests/default.pp
+++ centos6_puppet/puppet/environment/development/manifests/default.pp
@@ -8,11 +8,11 @@ file { '/etc/localtime':
 }
 exec { 'selinux':
     command     => '/usr/sbin/setenforce 0; /bin/sed -i -e "s|^SELINUX=.*$|SELINUX=disabled|" /etc/selinux/config',
-    unless      => '/usr/bin/test \! -f /etc/selinux/config || grep "SELINUX=disabled" /etc/selinux/config',
+    unless      => '/usr/bin/test \! -f /etc/selinux/config || /bin/grep "SELINUX=disabled" /etc/selinux/config',
 }
 exec { 'grub':
     command     => '/bin/sed -i -e "s|quiet.*$|quiet enforcing=0|" /etc/grub.conf',
-    unless      => '/usr/bin/test \! -f /etc/grub.conf || grep "quiet enforcing=0" /etc/grub.conf',
+    unless      => '/usr/bin/test \! -f /etc/grub.conf || /bin/grep "quiet enforcing=0" /etc/grub.conf',
 }
 service { 'iptables':
     enable      => false,
--- centos7_puppet/puppet/environment/development/manifests/default.pp
+++ centos7_puppet/puppet/environment/development/manifests/default.pp
@@ -1,6 +1,6 @@
 exec { 'locale':
     command     => '/bin/localectl set-locale LANG=ja_JP.UTF-8',
-    unless      => '/usr/bin/test \! -f /etc/locale.conf || /bin/grep "^LANG=\"ja_JP.UTF-8\"" /etc/locale.conf',
+    unless      => '/usr/bin/test \! -f /etc/locale.conf || /bin/grep "^LANG=ja_JP.UTF-8" /etc/locale.conf',
 }
 file { '/etc/localtime':
     ensure      => file,
@@ -8,7 +8,7 @@ file { '/etc/localtime':
 }
 exec { 'selinux':
     command     => '/usr/sbin/setenforce 0; /bin/sed -i -e "s|^SELINUX=.*$|SELINUX=disabled|" /etc/selinux/config',
-    unless      => '/usr/bin/test \! -f /etc/selinux/config || grep "SELINUX=disabled" /etc/selinux/config',
+    unless      => '/usr/bin/test \! -f /etc/selinux/config || /bin/grep "SELINUX=disabled" /etc/selinux/config',
 }
 service { 'firewalld':
     enable      => false,
